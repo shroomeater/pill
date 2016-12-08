853d2621a0fdb14fa98f2a5b07bebcab83b5c389
Salvatore <pigrecoinfinito@gmail.com>
2016-12-08 19:14:30 +0100

    Create pti_pill.sql

---
+++ pti_pill.sql
@@ -0,0 +1,22 @@
+CREATE TABLE "pti_pill" 
+(
+"pk_uid" integer NOT NULL,
+"rif_quadro" text ,
+"fraz_loc" text ,
+"data_ril" double ,
+"path_supporto" text ,
+"path_armatura" text ,
+"tipo" text ,
+"materiale" text ,
+ "stato" text ,
+"proprieta" text ,
+"nro_sorgenti" integer ,
+"lungh_sbraccio" double ,
+"potenza" integer ,
+"tipo_lampada" text ,
+"tipo_armatura" text ,
+"stato_sorgente" text ,
+"altezza_pl" double ,
+"cod_lampada" integer ,
+PRIMARY KEY ("pk_uid")
+)
