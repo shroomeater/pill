47294c9cfd0897d2b7b4b4c8539d3d46051c3a7f
Salvatore <pigrecoinfinito@gmail.com>
2017-03-30 19:18:04 +0200

    registrare view

--- v_count_sum.sql
+++ v_count_sum.sql
@@ -3,3 +3,8 @@ SELECT a.pk_uid, count(*) as tot_nro_pill,sum (nro_sorgenti) as tot_nro_sorgenti
 from pti_pill p, plg_aree_quadri a
 where st_intersects (p.geom, a.geom) =1
 group by a.pk_uid
+
+-- registrare vista
+INSERT INTO views_geometry_columns
+ (view_name, view_geometry, view_rowid, f_table_name, f_geometry_column, read_only)
+ VALUES ('v_count_sum', 'geom', 'pk_uid', 'plg_aree_quadri', 'geom',1)
